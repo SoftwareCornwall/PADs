272bc7a80499f008d1912cac69f10a3f87d007eb
Jon <jonathanpetercole@gmail.com>
2016-07-13 17:00:07 +0000

    Added .htaccess, added post functionality

--- web/index.php
+++ web/index.php
@@ -1,2 +1,12 @@
 <?php
-require 'vendor/autoload.php;
+require 'vendor/autoload.php';
+$app = new \Slim\App();
+
+// Define app routes
+$app->post('/event/', function ($request, $response) {
+    $data = $request->getParsedBody()['cabinet_id'];
+    return $response->write($data);
+});
+
+// Run app
+$app->run();
