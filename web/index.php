4966e72ba46be52e10225c53eb1a886679966350
Emma Duncan <emma16mae@gmail.com>
2016-07-13 15:55:49 +0000

    Wrote get and post functions for event

--- web/index.php
+++ web/index.php
@@ -1,3 +1,21 @@
-hello
 <?php 
 require 'vendor/autoload.php';
+$app = new \Slim\App();
+
+$app->get('/hello/{name}', function($request, $response, $args) {
+	return $response->write("Hello ".$args['name']);
+});
+
+$app->get('/bye/{name}', function($request, $response, $args) {
+	return $response->write("Bye ".$args['name']);
+});
+
+$app->post('/event', function ($request, $response, $args) {
+	$data = $request->getParsedBody(); //creates array from data posted by user
+	return $response->write($data["abc"]);
+
+});
+
+$app->run();
+
+?>
