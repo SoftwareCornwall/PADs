89ec54df13baf255d4ec46723cb61b63d12c0953
ChrisPawley <christopher.pawley@gmail.com>
2016-07-13 16:26:12 +0000

    removed commented code

--- Cabinet/WiringPiTest/main.cpp
+++ Cabinet/WiringPiTest/main.cpp
@@ -6,11 +6,6 @@ int main (void)
   wiringPiSetup() ;
   pinMode (0, INPUT) ;
   int i = 0;
- /* for (int i=0; i<20;i++)
-  {
-    digitalWrite (0, HIGH) ; delay (500) ;
-    digitalWrite (0,  LOW) ; delay (500) ;
-  }*/
 
   while(1)
   {
