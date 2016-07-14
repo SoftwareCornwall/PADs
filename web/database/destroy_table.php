6a432cf1109798b3ac9361d73a52d77a91df4480
Jon <jonathanpetercole@gmail.com>
2016-07-14 12:23:07 +0000

    Added a script that deletes all items in the single table in the DB.

---
+++ web/database/destroy_table.php
@@ -0,0 +1,26 @@
+<?php
+	// Connect to the database and declare the table name
+	include_once 'dbconnect.php';
+
+	// Delete Table With Post
+	if ($_GET["confirm"]=="y")
+	{
+		// Delete the table
+		$sql = "DELETE FROM tbl_cabinet";
+
+		// Check if the table creation was successful, and display an appropriate message
+		if ($conn->query($sql) === TRUE) {
+			echo "<br>Table ".$tblName." cleared successfully";
+		} else {
+			echo "Error clearing table: " . $conn->error;
+		}
+	}
+	else
+	{
+		// Form to Delete
+		echo "<form id='confirm'><button type='submit' name='confirm' value='y'>Confirm</button></form>";
+	}
+
+	// Close the connection
+	$conn->close();
+?>
