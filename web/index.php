86b36595680df474f03e5ede9a0a06da4160c3b6
Emma Duncan <emma16mae@gmail.com>
2016-07-14 11:25:59 +0000

    added code to call twig template (commented out)

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 require 'vendor/autoload.php';
 $app = new \Slim\App();
 
+
 $app->get('/hello/{name}', function($request, $response, $args) {
 	return $response->write("Hello ".$args['name']);
 });
@@ -14,6 +15,22 @@ $app->post('/event', function ($request, $response, $args) {
 	$data = $request->getParsedBody(); //creates array from data posted by user
 	return $response->write($data["abc"]);
 
+
+// Get container
+$container = $app->getContainer();
+
+/* Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('home/pi/PADs/web/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+*/
 });
 
 $app->run();
