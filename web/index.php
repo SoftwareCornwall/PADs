8eb5c1533ac29b7f22b0339a0e9888adaf00862e
Emma Duncan <emma16mae@gmail.com>
2016-07-14 15:44:02 +0100

    added basic templating files

--- web/index.php
+++ web/index.php
@@ -1,12 +1,46 @@
-<?php
+<?php 
 require 'vendor/autoload.php';
 $app = new \Slim\App();
 
-// Define app routes
-$app->post('/event/', function ($request, $response) {
-    $data = $request->getParsedBody()['cabinet_id'];
-    return $response->write($data);
+
+$app->get('/hello/{name}', function($request, $response, $args) {
+	return $response->write("Hello ".$args['name']);
 });
 
-// Run app
+$app->get('/bye/{name}', function($request, $response, $args) {
+	return $response->write("Bye ".$args['name']);
+});
+
+$app->post('/event', function ($request, $response, $args) {
+	$data = $request->getParsedBody(); //creates array from data posted by user
+	return $response->write($data["abc"]);
+});
+
+// Get container
+$container = $app->getContainer();
+
+//Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('./templates', [
+//        'cache' => 'home/pi/PADs/web/cache'
+        'cache' => false
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container['router'],
+        $container['request']->getUri()
+    ));
+
+    return $view;
+
+};
+
+// Render Twig template in route
+$app->get('/event/{id}', function ($request, $response, $args) {
+    return $this->view->render($response, 'sample.html', [
+        'id' => $args['id'] ]);
+
+});
 $app->run();
+
+
