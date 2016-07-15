95a268a80f1c32a5417e48e86d7c4c8d8ef06ef8
Jon Cole <JonathanPeterCole@gmail.com>
2016-07-15 16:00:13 +0100

    Corrected some capitalisation in the database design file and updated the tables_create file for the new database.

--- web/database/tables_create.php
+++ web/database/tables_create.php
@@ -1,20 +1,49 @@
 <?php
-	// Connect to the database and declare the table name
+	// Connect to the database
 	include_once 'db_connect.php';
-	$tblName = "tbl_cabinets";
+	$errors_occurred = 0;
 
-	// Create an SQL table with the set name and three data fields, including the ID, cabinet name and cabinet status
-	$sql = "CREATE TABLE ".$tblName." (
+	// Define cabinets table
+	$sql = "CREATE TABLE tbl_cabinets (
 		id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-		name VARCHAR(200) NOT NULL,
-		status TINYINT(1) NOT NULL
+		location VARCHAR(200) NOT NULL,
+		postcode VARCHAR(200) NOT NULL,
+		door_status VARCHAR(200) NOT NULL,
+		defig_status VARCHAR(200) NOT NULL,
+		last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 	)";
 
-	// Check if the table creation was successful, and display an appropriate message
+	// Create cabinets table
 	if ($conn->multi_query($sql) === TRUE) {
-		echo "<br>Table ".$tblName." created successfully";
+		echo "<p>Cabinets table created successfully.</p>";
 	} else {
-		echo "Error creating table: " . $conn->error;
+		echo "<p>Cabinets table creation failed.</br>";
+		echo "Error: " . $conn->error . "</p>";
+		$errors_occurred = 1;
+	}
+
+	// Define guardians table
+	$sql = "CREATE TABLE tbl_guardians (
+		id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		first_name VARCHAR(200) NOT NULL,
+		last_name VARCHAR(200) NOT NULL,
+		phone_number VARCHAR(200) NOT NULL,
+		cabinet_id INT NOT NULL
+	)";
+
+	// Create guardians table
+	if ($conn->multi_query($sql) === TRUE) {
+		echo "<p>Guardians table created successfully.</p>";
+	} else {
+		echo "<p>Guardians table creation failed.</br>";
+		echo "Error: " . $conn->error . "</p>";
+		$errors_occurred = 1;
+	}
+
+	// If errors occurred
+	if ($errors_occurred == 1) {
+		echo "<p>One or more of the tables could not be created.</br>";
+		echo "Please ensure that you have created the database first.</p>";
 	}
 
 	// Close the connection
