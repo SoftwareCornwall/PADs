dc221ea77c9b72cbdca18c65174cf8ca8277778c
Emma Duncan <emma16mae@gmail.com>
2016-07-18 15:34:20 +0100

    committed action.php

--- web/templates/action.php
+++ web/templates/action.php
@@ -1,12 +1,14 @@
-<!html>
+<html>
+<body>
 
-<!>
+
+</body>
+</html>
  
 <?php
-echo "<h2>PHP is Fun!</h2>";
+
 echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
 ?> 
 
 
