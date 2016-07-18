09ee6977808d1583d9683f815189af53c9665224
Emma Duncan <emma16mae@gmail.com>
2016-07-18 14:14:27 +0100

    Set up forms for web page

---
+++ web/templates/action.php
@@ -0,0 +1,12 @@
+<!html>
+
+<!>
+ 
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+
