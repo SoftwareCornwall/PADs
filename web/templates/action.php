607bddb8122a3f83da32db6ca12534c477755b0a
Emma Duncan <emma16mae@gmail.com>
2016-07-19 16:52:39 +0100

    changed action.php - now can add to database

--- web/templates/action.php
+++ web/templates/action.php
@@ -1,7 +1,10 @@
 <html>
 <body>
 New PAD Location: <?php echo $_POST["pad_l"]; ?><br>
-New PAD Postcode:  <?php echo $_POST["pad_id"]; ?>
+New PAD Postcode:  <?php echo $_POST["pad_id"]; ?><br>
+New PAD Postcode:  <?php echo $_POST["pad_post"]; ?><br>
+New PAD Postcode:  <?php echo $_POST["pad_door"]; ?><br>
+New PAD Postcode:  <?php echo $_POST["pad_defib"]; ?>
 <br><br>
 
 </body>
@@ -14,8 +17,8 @@ $sql = "";
 
 	 
 	// Insert new row with fake details
-	$sql .= "INSERT INTO tbl_cabinets (location, postcode)
-	VALUES ('".$_POST["pad_id"]."','".$_POST["pad_l"].",'".$_POST["pad_post"].",'".$_POST["pad_door"].",'".$_POST["pad_defib"]."');";
+	$sql .= "INSERT INTO tbl_cabinets (id, location, postcode)
+	VALUES ('".$_POST["pad_id"]."','".$_POST["pad_l"].",'".$_POST["pad_post"]."');";
 	//echo "$sql";
 	
 
@@ -26,6 +29,23 @@ $sql = "";
 		// If there was an error, display a message at the top of the page
 		echo "New entries could not be added because of an error " . mysqli_error($conn);
 	}
+
+/*,'".$_POST["pad_door"].",'".$_POST["pad_defib"]."'
+	// Insert new row with fake details
+	$sql .= "INSERT INTO tbl_status (cabinet_id, postcode)
+	VALUES ('".$_POST["pad_id"]."','".$_POST["pad_l"].",'".$_POST["pad_post"]."');";
+	//echo "$sql";*/
+	//
+//REWRITE FOR STATUS TABLE, LAST 2 VALUES
+
+	/* Check if it was successful
+	if ($res=mysqli_multi_query($conn, $sql)) {
+		//echo "Successful. ";
+	} else {
+		// If there was an error, display a message at the top of the page
+		echo "New entries could not be added because of an error " . mysqli_error($conn);
+	}
+*/
 
 $conn->close();
 ?> 
