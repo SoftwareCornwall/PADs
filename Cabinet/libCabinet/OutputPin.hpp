a08fc55843fd9ecfcbe3f6ec53d017f7dd5e4d4d
Keith Eli <keitheli@hotmail.com>
2016-07-19 10:47:22 +0000

    Removed code in OutputPin interface

--- Cabinet/libCabinet/OutputPin.hpp
+++ Cabinet/libCabinet/OutputPin.hpp
@@ -6,11 +6,6 @@ class OutputPin
 public:
     virtual ~OutputPin() { };
     virtual void State(bool state) = 0;
-    virtual bool getState() {return pinState;};
-
-protected:
-    bool pinState = false;
-
 };
 
 #endif // OUTPUTPIN_HPP
