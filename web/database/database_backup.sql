3dfb8518d7fe28f6f5c4c75a2aa69e9959da30a9
Jon Cole <JonathanPeterCole@gmail.com>
2016-07-21 16:06:39 +0100

    Backup of current data.

--- web/database/database_backup.sql
+++ web/database/database_backup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2016 at 01:48 PM
+-- Generation Time: Jul 21, 2016 at 05:04 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -67,6 +67,19 @@ INSERT INTO `tbl_cabinets` (`id`, `location`, `postcode`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_fleet`
+--
+
+CREATE TABLE `tbl_fleet` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `phone_number` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_guardians`
 --
 
@@ -104,6 +117,25 @@ CREATE TABLE `tbl_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tbl_status`
+--
+
+INSERT INTO `tbl_status` (`id`, `cabinet_id`, `door_status`, `defib_status`, `hanger_status`, `alarm_status`, `temp_status`, `last_update`) VALUES
+(90, 'AA123', 'Closed', '', 'Down', 'Working', 14, '2016-07-21 11:49:43'),
+(91, 'AA123', 'Open', '', 'Down', 'Working', 14, '2016-07-21 11:50:02'),
+(92, 'AA123', 'Open', '', 'Down', 'Working', 14, '2016-07-21 13:10:49'),
+(93, 'AA123', 'Open', '', 'Down', 'Working', 14, '2016-07-21 13:35:57'),
+(94, 'AA123', 'Open', '', 'Down', 'Working', 14, '2016-07-21 13:37:54'),
+(95, 'AA123', 'Closed', '', 'Down', 'Working', 14, '2016-07-21 14:03:36'),
+(100, 'AA123', 'Open', '', 'Down', 'Working', 14, '2016-07-21 14:15:29'),
+(102, 'AA123', 'Open', '', 'Up', 'Working', 14, '2016-07-21 14:16:57'),
+(103, 'AA123', 'Open', '', 'Up', 'Fault', 14, '2016-07-21 14:17:06'),
+(104, 'AA123', 'Open', '', 'Up', 'Fault', 14, '2016-07-21 14:18:26'),
+(105, 'AA123', 'Closed', '', 'Down', 'Working', 14, '2016-07-21 14:18:43'),
+(106, 'AA123', 'Closed', '', 'Down', 'Working', 14, '2016-07-21 14:43:22'),
+(107, 'AA123', 'Open', '', 'Down', 'Working', 14, '2016-07-21 14:43:32');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -111,6 +143,12 @@ CREATE TABLE `tbl_status` (
 -- Indexes for table `tbl_cabinets`
 --
 ALTER TABLE `tbl_cabinets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_fleet`
+--
+ALTER TABLE `tbl_fleet`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -132,6 +170,11 @@ ALTER TABLE `tbl_status`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_fleet`
+--
+ALTER TABLE `tbl_fleet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `tbl_guardians`
 --
 ALTER TABLE `tbl_guardians`
@@ -140,7 +183,7 @@ ALTER TABLE `tbl_guardians`
 -- AUTO_INCREMENT for table `tbl_status`
 --
 ALTER TABLE `tbl_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 --
 -- Constraints for dumped tables
 --
