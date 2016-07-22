1d73cf2bb3ade36c60976db7eb20f054d21b3bff
Emma Duncan <emma16mae@gmail.com>
2016-07-22 10:49:47 +0100

    fixed cut off time problem

--- web/index.php
+++ web/index.php
@@ -1,12 +1,11 @@
 <?php 
 
-use \Psr\Http\Message\ServerRequestInterface as Request;//shortens path to 'request'
-use \Psr\Http\Message\ResponseInterface as Response;//^^to 'Response'
+use \Psr\Http\Message\ServerRequestInterface as Request;	//shortens path to 'request'
+use \Psr\Http\Message\ResponseInterface as Response;		//^^to 'Response'
+
 require 'vendor/autoload.php';
 $app = new \Slim\App(); 
 
 
 ?>
-
-
-should redirect to dashboard....
+<--should redirect to dashboard....
