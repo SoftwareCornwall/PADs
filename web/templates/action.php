1026fa55bccc2a3de2986e6200bc625d08abbbc2
Emma Duncan <emma16mae@gmail.com>
2016-07-22 09:23:07 +0100

    Tidied up code

--- web/templates/action.php
+++ web/templates/action.php
@@ -1,11 +1,12 @@
 <html>
 <body>
-<!--New PAD ID:  <?php echo $_POST["pad_id"]; ?><br>
+New PAD ID:  <?php echo $_POST["pad_id"]; ?><br>
 New PAD Location: <?php echo $_POST["pad_l"]; ?><br>
 New PAD Postcode:  <?php echo $_POST["pad_post"]; ?><br>
 New PAD Door Status:  <?php echo $_POST["pad_door"]; ?><br>
-New PAD Defib Status:  <?php echo $_POST["pad_defib"]; ?>-->
-<br><br>
+New PAD Defib Status:  <?php echo $_POST["pad_defib"]; ?><br>
+<!-- shows user data entered on form-->
+<br>
 
 </body>
 </html>
@@ -14,7 +15,6 @@ New PAD Defib Status:  <?php echo $_POST["pad_defib"]; ?>-->
 include_once '../database/db_connect.php';
 
 $sql = "";
-
 	 
 	// Insert new row with fake details
 	//$sql .= "INSERT INTO tbl_cabinets (id, location, postcode) VALUES ("a","b","c");
@@ -28,15 +28,8 @@ $sql = "";
 	$sql = "INSERT INTO tbl_cabinets (id, location, postcode) VALUES ('$pad_id', '$pad_l', '$pad_post');"; 
 	$sql .= "INSERT INTO tbl_status (cabinet_id, door_status, defib_status) VALUES ('$pad_id', '$pad_door', '$pad_defib');";
 	
-
 	//add values to tbl_status
-
 	
-	//$sql .= "INSERT INTO tbl_status (id, door_status, defib_status) VALUES ('$cabinet_id', '$pad_door', '$pad_defib');";
-
-	/*$sql .= "INSERT INTO tbl_status (id, door_status, defib_status)
-	VALUES ('".$_POST["pad_id"]."','".$_POST["pad_door"].",'".$_POST["pad_defib"]."');";*/
-
 	// Check if it was successful
 	if ($res=mysqli_multi_query($conn, $sql)) {
 		//echo "Successful. ";
@@ -45,25 +38,10 @@ $sql = "";
 		echo "New entries could not be added because of an error " . mysqli_error($conn);
 	}
 
-/*,'".$_POST["pad_door"].",'".$_POST["pad_defib"]."'
-	// Insert new row with fake details
-	$sql .= "INSERT INTO tbl_status (cabinet_id, postcode)
-	VALUES ('".$_POST["pad_id"]."','".$_POST["pad_l"].",'".$_POST["pad_post"]."');";
-	//echo "$sql";*/
-	//
-//REWRITE FOR STATUS TABLE, LAST 2 VALUES
-
-	/* Check if it was successful
-	if ($res=mysqli_multi_query($conn, $sql)) {
-		//echo "Successful. ";
-	} else {
-		// If there was an error, display a message at the top of the page
-		echo "New entries could not be added because of an error " . mysqli_error($conn);
-	}
-*/
 
 $conn->close();
 ?> 
+
 <html>
-<meta http-equiv="refresh" content="30;URL=../../event/abc" />
+<meta http-equiv="refresh" content="5;URL=../../event/abc" />
 </html>
