cabe4fa6bc8ff3bc561d49284279c7a9c010dfb2
Emma Duncan <emma16mae@gmail.com>
2016-07-22 10:46:29 +0100

    improved css formatting + fixed create form bug

--- web/templates/action.php
+++ web/templates/action.php
@@ -1,23 +1,11 @@
-<html>
-<body>
-New PAD ID:  <?php echo $_POST["pad_id"]; ?><br>
-New PAD Location: <?php echo $_POST["pad_l"]; ?><br>
-New PAD Postcode:  <?php echo $_POST["pad_post"]; ?><br>
-New PAD Door Status:  <?php echo $_POST["pad_door"]; ?><br>
-New PAD Defib Status:  <?php echo $_POST["pad_defib"]; ?><br>
-<!-- shows user data entered on form-->
-<br>
 
-</body>
-</html>
  
 <?php
-include_once '../database/db_connect.php';
+	include_once '../database/db_connect.php';
 
-$sql = "";
+	$sql = "";
 	 
 	// Insert new row with fake details
-	//$sql .= "INSERT INTO tbl_cabinets (id, location, postcode) VALUES ("a","b","c");
 	$pad_id = $_POST["pad_id"];	
 	$pad_l = $_POST["pad_l"];
 	$pad_post = $_POST["pad_post"];	
@@ -33,15 +21,24 @@ $sql = "";
 	// Check if it was successful
 	if ($res=mysqli_multi_query($conn, $sql)) {
 		//echo "Successful. ";
-	} else {
+	} 
+	
+	else {
 		// If there was an error, display a message at the top of the page
 		echo "New entries could not be added because of an error " . mysqli_error($conn);
 	}
 
 
-$conn->close();
+	$conn->close();
 ?> 
 
 <html>
-<meta http-equiv="refresh" content="5;URL=../../event/abc" />
+	<meta http-equiv="refresh" content="3;URL=../../event/abc" />
+
+	<body>
+	<br><br>New PAD ID: <?php echo $_POST["pad_id"]; ?> <br>
+			New PAD Location: <?php echo $_POST["pad_l"]; ?> <br>
+			New PAD Postcode: <?php echo $_POST["pad_post"]; ?> <br>
+			New PAD Door Status: <?php echo $_POST["pad_door"]; ?> <br>
+			New PAD Defibrillator Status: <?php echo $_POST["pad_defib"]; ?> <br>
 </html>
